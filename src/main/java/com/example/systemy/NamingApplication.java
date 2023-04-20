dd9c29ddefa38a3a81c75368f623649df482eb21
joshu <joshua.eijbersen@htomail.com>
2023-04-20 11:39:35 +0200

    Update 1.5 20/04/2023

---
+++ src/main/java/com/example/systemy/NamingApplication.java
@@ -0,0 +1,21 @@
+package com.example.systemy;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SpringBootApplication
+public class NamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServer.class, args);
+    }
+}
