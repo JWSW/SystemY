c1fb45e52a5800562b492abd8adc019ed8174411
joshu <joshua.eijbersen@htomail.com>
2023-04-29 21:28:25 +0200

    Update 2.35 29/04/2023 Node

--- src/main/java/com/example/systemy/Node.java
+++ src/main/java/com/example/systemy/Node.java
@@ -109,7 +109,7 @@ public class Node implements UnicastObserver{
                 .POST(HttpRequest.BodyPublishers.ofString(json))//"{nodeName:" + node.getNodeName() + "ipAddress:" + node.getIpAddress() + "}"))
                 .build();
         try{
-            System.out.println("Sending request to add node.");
+            System.out.println("Sending request to get new neighbour.");
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("Response: " + response.body());
         } catch (IOException | InterruptedException e) {
