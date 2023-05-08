c7b9d90ad7bbbe81b3f0ee0098c761df0534093c
Abdelkarim555 <abdelkarim.sallami@hotmail.com>
2023-05-08 10:42:14 +0200

    optimize

--- src/main/java/com/example/systemy/HeartbeatSender.java
+++ src/main/java/com/example/systemy/HeartbeatSender.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.net.*;
 
 public class HeartbeatSender extends Thread{
-    private int port;
+    private final int port;
     private int currentID;
-    private String sendIP;
-    private InetAddress sendAddress;
+    private final String sendIP;
     DatagramPacket sendPacket;
-    private boolean stopping = false;
     protected byte[] buf = new byte[256];
 
     public HeartbeatSender(String sendIP, int currentID, int port) {
@@ -18,31 +16,15 @@ public class HeartbeatSender extends Thread{
         this.sendIP = sendIP;
     }
 
-    public void setPort(int port) {
-        this.port = port;
-    }
-
     public void setCurrentID(int currentID) {
         this.currentID = currentID;
-    }
-
-    public void setSendingIP(String sendigIP) throws UnknownHostException {
-        this.sendIP = sendigIP;
-        sendAddress = InetAddress.getByName(sendigIP);
-        sendPacket = new DatagramPacket(buf, buf.length, sendAddress, port);
-        System.out.println("Changed sending parameters: " + this.sendIP + ", " + sendAddress + ", " + sendPacket);
-    }
-
-    public void stopSending(){
-        stopping = true;
     }
 
     @Override
     public void run() {
         try {
-            stopping = false;
             DatagramSocket socket;
-            sendAddress = InetAddress.getByName(sendIP);
+            InetAddress sendAddress = InetAddress.getByName(sendIP);
             socket = new DatagramSocket();
 
             String message = String.valueOf(currentID);
@@ -53,11 +35,6 @@ public class HeartbeatSender extends Thread{
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.println("Sending ping to " + sendAddress + " or " + sendIP + " with port " + port);
                 socket.send(sendPacket);
-                if (stopping) {
-                    System.out.println("HeartbeatSender has stopped.");
-                    break;
-                }
-
                 sleep(10000);
             }
         } catch (InterruptedException | IOException e) {
