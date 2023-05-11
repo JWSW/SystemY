6ca596db06f7d71ed4819ddda3b28107f71a2275
joshu <joshua.eijbersen@htomail.com>
2023-05-11 12:25:52 +0200

    Update 3.0.6 11/05/2023 Node

--- src/main/java/com/example/systemy/TCPReceiver.java
+++ src/main/java/com/example/systemy/TCPReceiver.java
@@ -47,7 +47,7 @@ public class TCPReceiver extends Thread {
         try {
             serverSocket = new ServerSocket(port);
             Socket socket = serverSocket.accept();
-            System.out.println("Connection accepted");
+//            System.out.println("Connection accepted");
             isAccepted = true;
             InputStream inputStream = socket.getInputStream();
             FileOutputStream fileOutputStream = new FileOutputStream(fileName);
