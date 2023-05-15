c14b7a17c82e786039e57902158d20873569d0a3
joshu <joshua.eijbersen@htomail.com>
2023-05-15 11:18:51 +0200

    Update 3.1.0 15/05/2023 Node: Start REST communication

--- src/main/java/com/example/systemy/Controller.java
+++ src/main/java/com/example/systemy/Controller.java
@@ -6,7 +6,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/requestNode")
 public class Controller {
     @Autowired
     private Services services;
+
+    @PostMapping("/{filename}/sendNewFile")
+    public void sendNewFile (@PathVariable String filename, @RequestBody Node ){
+        services.
+    }
+
+
+    @GetMapping("/syncWithNeighbor")
+    public Map<String,Boolean> syncWithNeighbor(){
+
+    }
 }
