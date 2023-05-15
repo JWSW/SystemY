9e1c3a78ab08fe7394ef2fd775f3b9db89cd0caa
joshu <joshua.eijbersen@htomail.com>
2023-05-15 17:10:49 +0200

    Update 3.1.4 15/05/2023 Node: 'juiste' manier json

--- src/main/java/com/example/systemy/Controller.java
+++ src/main/java/com/example/systemy/Controller.java
@@ -1,8 +1,11 @@
 package com.example.systemy;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -12,7 +15,17 @@ public class Controller {
     private Services services;
 
     @PostMapping("/{filename}/{nodeID}/{nodeIP}/sendNewFile")
-    public void sendNewFile (@PathVariable String filename, @PathVariable String nodeID, @PathVariable String nodeIP, @RequestBody String base64Content){
+    public void sendNewFile (@PathVariable String filename, @PathVariable String nodeID, @PathVariable String nodeIP, @RequestBody String jsonData){
+        String base64Content="";
+        try {
+            // Parse the JSON string to a JSON object
+            JSONObject jsonObject = new JSONObject(jsonData);
+
+            // Extract the file data field from the JSON object
+            base64Content = jsonObject.getString("fileData");
+        }catch (JSONException e) {
+            System.out.println("Error receiving file: " + e.getMessage());
+        }
         services.setNewFile(filename, base64Content, Integer.parseInt(nodeID), nodeIP);
     }
 
