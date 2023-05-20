931900835a00c97214772ededa8f067cb9171177
Abdelkarim555 <abdelkarim.sallami@hotmail.com>
2023-05-20 17:34:51 +0200

    Sync agent debug5

--- src/main/java/com/example/systemy/FileChecker.java
+++ src/main/java/com/example/systemy/FileChecker.java
@@ -54,6 +54,7 @@ public class FileChecker extends Thread {
 
                 // Remove files that are no longer being edited from the map
                 files.entrySet().removeIf(entry -> !entry.getValue());
+                System.out.println("files:"+files);
             }
 
 
