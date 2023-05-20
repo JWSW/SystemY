5e4d4d540d94fbb096716ef63048f12cc178b94d
Abdelkarim555 <abdelkarim.sallami@hotmail.com>
2023-05-20 14:03:13 +0200

    Sync agent debug5

--- src/main/java/com/example/systemy/FileChecker.java
+++ src/main/java/com/example/systemy/FileChecker.java
@@ -14,25 +14,31 @@ public class FileChecker extends Thread {
     @Override
     public void run() {
         try {
+            while (true) {
             for (String directory : directories) {
-                Process process = Runtime.getRuntime().exec("lsof +D " + directory);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line;
-                boolean isBeingEdited = false;
-                System.out.println("test");
-                while ((line = reader.readLine()) != null) {
-                    // Check if the line contains the file information
-                    if (line.contains("nano") || line.contains("vi") || line.contains("vim")) {
-                        isBeingEdited = true;
-                        break; // Exit the loop once the file is found being edited
-                    }
-                }
-                reader.close();
+                 Process process = Runtime.getRuntime().exec("lsof +D " + directory);
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                 String line;
+                 boolean isBeingEdited = false;
+                    if ((line = reader.readLine()) != null) {
+                        // Check if the line contains the file information
+                        if (line.contains("nano") || line.contains("vi") || line.contains("vim")) {
+                            String[] tokens = line.split("\\s+");
+                            String fileName = tokens[tokens.length - 1];
 
-                if (isBeingEdited) {
-                    System.out.println("The file is being edited.");
-                } else {
-                    System.out.println("The file is not being edited.");
+                            System.out.println("Open file: " + fileName);
+                            // Implement your lock or unlock logic here based on the file name
+
+                            isBeingEdited = true;
+                        }
+                    }
+                    reader.close();
+
+                    if (isBeingEdited) {
+                        System.out.println("The file is being edited.");
+                    } else {
+                        System.out.println("The file is not being edited.");
+                    }
                 }
             }
         } catch (IOException e) {
