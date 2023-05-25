9a18a80813d9df5a5561169385877f5d6cf5ce72
Joshua Eijbersen <joshua.eijbersen@student.uantwerpen.be>
2023-05-25 11:45:33 +0200

    Update 3.2.27 25/05/2023 Node: changed delay to 300ms

--- src/main/java/SystemY/Node.java
+++ src/main/java/SystemY/Node.java
@@ -297,7 +297,7 @@ public class Node implements Observer {
             e.printStackTrace();
         }
         try {
-            TimeUnit.MILLISECONDS.sleep(500);
+            TimeUnit.MILLISECONDS.sleep(300);
             HttpRequest request2 = HttpRequest.newBuilder()
                     .uri(URI.create("http://" + nodeIP + ":8081/requestNode" + "/" + filename + "/" + currentID + "/" + ipAddress + "/sendNewFile"))
                     .header("Content-Type", "application/json")
