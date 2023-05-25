059ee6076ca691f6bba249a042d4c7126bd5780a
Abdelkarim555 <abdelkarim.sallami@hotmail.com>
2023-05-25 12:41:03 +0200

    failure agent sent/received test

--- src/main/java/SystemY/Agents/FailureAgent.java
+++ src/main/java/SystemY/Agents/FailureAgent.java
@@ -92,7 +92,7 @@ public class FailureAgent implements Runnable, Serializable {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        currentNode.setOwnerFile(filename, Integer.parseInt(ownerNode),nodeIP);
+        //currentNode.setOwnerFile(filename, Integer.parseInt(ownerNode),nodeIP);
     }
 
     private void passFailureAgentToNextNode() throws IOException, InterruptedException {
