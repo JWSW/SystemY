36ef4ec9fcb61bcdb144de985e9356a3d038822d
Joshua Eijbersen <joshua.eijbersen@student.uantwerpen.be>
2023-05-25 11:27:17 +0200

    Update 3.2.25 25/05/2023 Node: Added delay to prevent overloading node with requests

--- src/main/java/SystemY/Node.java
+++ src/main/java/SystemY/Node.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse;
 import java.nio.file.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import SystemY.interfaces.Observer;
 import SystemY.Agents.FailureAgent;
@@ -296,6 +297,7 @@ public class Node implements Observer {
             e.printStackTrace();
         }
         try {
+            TimeUnit.SECONDS.sleep(100);
             HttpRequest request2 = HttpRequest.newBuilder()
                     .uri(URI.create("http://" + nodeIP + ":8081/requestNode" + "/" + filename + "/" + currentID + "/" + ipAddress + "/sendNewFile"))
                     .header("Content-Type", "application/json")
